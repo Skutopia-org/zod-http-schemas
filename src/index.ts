eef47e760fba790abfc41961dc1069256bb7a530
Troy Gerwien <yortus@gmail.com>
2020-02-17 15:10:09 +0800

    export more types from rtti

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-import {t} from 'rtti';
-import {createHttpClient, HttpClient, HttpClientOptions} from './create-http-client';
-import {decorateExpressServer, DecoratedExpressServer, RequestHandler} from './decorate-express-server';
-import {createHttpRoute, createHttpSchema} from './create-http-schema';
-
-
-export {t, createHttpRoute, createHttpSchema};
-export {createHttpClient, HttpClient, HttpClientOptions};
-export {decorateExpressServer, DecoratedExpressServer, RequestHandler};
+export {t, TypeFromTypeInfo, TypeInfo} from 'rtti';
+export {createHttpClient, HttpClient, HttpClientOptions} from './create-http-client';
+export {createHttpRoute, createHttpSchema} from './create-http-schema';
+export {decorateExpressServer, DecoratedExpressServer, RequestHandler} from './decorate-express-server';
